1b495f3552a5e17600f2b2dec21cbefefb3e7780
Sajid231 <marufbillah1020@gmail.com>
2021-09-02 03:42:20 +0600

    Lecture 11
    Implementing Error and Success Messages

--- 18-forkify/starter/src/js/controller.js
+++ 18-forkify/starter/src/js/controller.js
@@ -1,11 +1,13 @@
 import * as model from './model.js';
+import recipeView from './views/recipeView.js';
+import searchView from './views/searchView.js';
 // import recipeView from './views/recipeView';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
-import recipeView from './views/recipeView';
+import { async } from 'regenerator-runtime/runtime';
 
-const recipeContainer = document.querySelector('.recipe');
+// const recipeContainer = document.querySelector('.recipe');
 
 // https://forkify-api.herokuapp.com/v2
 
@@ -14,7 +16,7 @@ const recipeContainer = document.querySelector('.recipe');
 const controlRecipes = async function () {
   try {
     const id = window.location.hash.slice(1);
-    console.log(id);
+    // console.log(id);
 
     if (!id) return;
     recipeView.randerSpinner();
@@ -32,8 +34,26 @@ const controlRecipes = async function () {
   }
 };
 
+const controlSearchResult = async function () {
+  try {
+    // 1) Get search query
+    const query = searchView.getQuery();
+    if (!query) return;
+
+    // 2) load search results
+    await model.loadSearchResults(query);
+
+    // 3) render result
+    console.log(model.state.search.results);
+  } catch (err) {
+    console.log(err);
+  }
+};
+controlSearchResult();
+
 const init = function () {
   recipeView.addHandlerRander(controlRecipes);
+  searchView.addHandelerSearch(controlSearchResult);
 };
 init();
 // controlRecipes();
--- 18-forkify/starter/src/js/model.js
+++ 18-forkify/starter/src/js/model.js
@@ -4,11 +4,15 @@ import { getJSON } from './helpers';
 
 export const state = {
   recipe: {},
+  search: {
+    query: '',
+    results: [],
+  },
 };
 
 export const loadRecipe = async function (id) {
   try {
-    const data = await getJSON(`${API_URL}/${id}`);
+    const data = await getJSON(`${API_URL}${id}`);
 
     const { recipe } = data.data;
     state.recipe = {
@@ -25,6 +29,28 @@ export const loadRecipe = async function (id) {
     console.log(state.recipe);
   } catch (err) {
     // temporaty error handeling
+    console.error(`${err} here 🔥`);
+    throw err;
+  }
+};
+
+export const loadSearchResults = async function (quary) {
+  try {
+    state.search.query = quary;
+
+    const data = await getJSON(`${API_URL}?search=${quary}`);
+    // console.log(data);
+
+    state.search.results = data.data.recipes.map(rec => {
+      return {
+        id: rec.id,
+        title: rec.title,
+        publisher: rec.publisher,
+        image: rec.image_url,
+      };
+    });
+    // console.log(state.search.results);
+  } catch (err) {
     console.error(`${err} here 🔥`);
     throw err;
   }
