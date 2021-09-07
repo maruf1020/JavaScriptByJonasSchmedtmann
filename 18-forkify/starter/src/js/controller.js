0d3e722e5022e7bacc68868cf55bebf483df0ea2
Sajid231 <marufbillah1020@gmail.com>
2021-09-08 04:21:34 +0600

    Lecture 18
    Developing a DOM Updating Algorithm

--- 18-forkify/starter/src/js/controller.js
+++ 18-forkify/starter/src/js/controller.js
@@ -28,6 +28,9 @@ const controlRecipes = async function () {
     if (!id) return;
     recipeView.randerSpinner();
 
+    // 0) update result view to mark selected search result
+    resultsView.update(model.getSearchResultPage());
+
     // 1) loading recepi
     await model.loadRecipe(id);
     // const { recipe } = model.state;
@@ -82,7 +85,8 @@ const controlServings = function (newServings) {
   model.updateServings(newServings);
 
   // Update the recipe view
-  recipeView.render(model.state.recipe);
+  // recipeView.render(model.state.recipe);
+  recipeView.update(model.state.recipe);
 };
 
 const init = function () {
--- 18-forkify/starter/src/js/views/resultsView.js
+++ 18-forkify/starter/src/js/views/resultsView.js
@@ -11,15 +11,21 @@ class ResultsView extends View {
     return this._data.map(this._generateMarkupPreview).join('');
   }
   _generateMarkupPreview(result) {
+    const id = window.location.hash.slice(1);
+
     return `
     <li class="preview">
-            <a class="preview__link" href="#${result.id}">
+            <a class="preview__link ${
+              result.id === id ? 'preview__link--active' : ''
+            }" href="#${result.id}">
               <figure class="preview__fig">
                 <img src="${result.image}" alt="${result.title}" />
               </figure>
               <div class="preview__data">
                 <h4 class="preview__title">${result.title}</h4>
-                <p class="preview__publisher">${result.publisher}</p>                
+                <p class="preview__publisher">${
+                  result.publisher
+                }</p>                
               </div>
             </a>
           </li>
